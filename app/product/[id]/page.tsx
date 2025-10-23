59f4c81b14de96c6cb9668e12840134da3667cc8
m7mdev1 <69204096+M7MDdev1@users.noreply.github.com>
2025-10-23 17:39:33 +0300

    reorder Proder page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -14,14 +14,20 @@ export default async function ProductDetails({
   const response = await fetch(url, { cache: "no-store" });
   const product: Iproduct = await response.json();
   return (
+    <div className="flex">
+      <div className="w-136 h-180 bg-[#F3F5F7] flex items-center justify-center">
+        <img src={product.image} alt={product.title} />
+      </div>
+      <div>
+        <h1 className="text-4xl font-medium">{product.title}</h1>
+        <p className="font-normal text-[#6C7275]">{product.description}</p>
+        <p className="text-3xl">${product.price}</p>
+        <p>category: {product.category}</p>
 
-    
-    <div>
-      <h1>{product.title}</h1>
-      <p>{product.description}</p>
-      <img src={product.image} alt={product.title} width={200} height={200} />
-      <p>Price: ${product.price}</p>
-      <p>category: ${product.category}</p>
+        <button className="bg-black text-white px-6 py-3 mt-4 rounded-md">
+          Add to Cart
+        </button>
+      </div>
     </div>
   );
 }
